7401831ac5a08a9427fff4ebff3f4bccbb8b5d24
Garima Deborah Zachariah <garimadeborahzachariah@Garimas-MacBook-Pro.local>
2022-01-28 14:05:52 -0500

    latest code for heroku

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 const app = express();
 
-app.get('/hello', (req, res) =>
+app.get('/hello', (req: Request, res: Response) =>
     res.send('Hello World!'));
 
+app.get('/add/:a/:b', (req: Request, res: Response) =>
+    res.send(req.params.a + req.params.b));
+
 const PORT = 4000;
-app.listen(PORT);
+app.listen(process.env.PORT || PORT);
