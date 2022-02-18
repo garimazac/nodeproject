36e13648af3a8af0e29ffa4f111546f80d4fd6ee
Garima Deborah Zachariah <garimadeborahzachariah@Garimas-MacBook-Pro.local>
2022-02-18 14:23:58 -0500

    Like Controller

--- server.ts
+++ server.ts
@@ -16,7 +16,8 @@
 import express, {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
-// import LikeController from "./controllers/LikeController";
+import LikeController from "./controllers/LikeController";
+
 import mongoose from "mongoose";
 
 // connect to the database
@@ -37,6 +38,7 @@ app.get('/add/:a/:b', (req: Request, res: Response) =>
 
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
+const likeController = LikeController.getInstance(app);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
